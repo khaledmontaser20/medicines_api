ca9a9a6e03229b5e91631de0c593a5ebdcc81e6c
khaledmontaser22 <khaled.montaser@geeksjo.com>
2023-03-06 22:29:51 +0200

    develop the cors

--- medicine_recognition_app/medicine_recognition_app/settings.py
+++ medicine_recognition_app/medicine_recognition_app/settings.py
@@ -139,16 +139,8 @@ REST_FRAMEWORK = {
 }
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:55384/",
+CORS_ORIGIN_ALLOW_ALL = False
 
-]
-
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:55384',
 ]
